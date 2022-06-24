9a00fa95be602885a09adf0fb518386fc12a495a
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2022-06-24 19:00:48 +0700

    firebase logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,7 +16,21 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text('Signed In'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Signed In'),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.deepPurple,
+                ),
+                child: Text("Sign Out"),
+              ),
+            ],
+          ),
         ),
       ),
     );
